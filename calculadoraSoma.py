4da75a4bc3c1231d35acd840bd839f63574617d7
Renato-Miranda <renatomna@gmail.com>
2025-06-24 21:20:54 -0300

    Criação de input para dados

--- calculadoraSoma.py
+++ calculadoraSoma.py
@@ -1,11 +1,20 @@
 def main():
 
-    def somar_numeros():
-        numero1 = 12
-        numero2 = 14
-        soma = numero1 + numero2 
-        print("A soma dos dois números é:", soma)
+    # def somar_numeros():
+    #     numero1 = 12
+    #     numero2 = 14
+    #     soma = numero1 + numero2 
+    #     print("A soma dos dois números é:", soma)
 
-    somar_numeros()
+    # somar_numeros()
+
+    def calc():
+        numero_1 = int(input("Digite o primeiro numero: "))
+        numero_2 = int(input("Digite o segundo numero: "))
+        soma1 = numero_1 + numero_2
+        print("A soma dos dois números é:", soma1)
+
+    calc()
 
 main()
+
