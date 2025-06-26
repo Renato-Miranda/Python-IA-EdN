1ace2344827058b54f9538f8c61cd63215253030
Renato-Miranda <renatomna@gmail.com>
2025-06-26 20:40:35 -0300

    criação calculadora de combustivel

---
+++ Atividades_02/calc-combustivel.py
@@ -0,0 +1,16 @@
+def calcular_consumo():
+   
+    distancia = 300  
+    combustivel = 25  
+
+    consumo_medio = distancia / combustivel
+
+    print("=== Relatório da Viagem ===")
+    print(f"Distância percorrida: {distancia} km")
+    print(f"Combustível gasto: {combustivel} litros")
+    print(f"Consumo médio: {consumo_medio:.2f} km/l")
+
+
+def main():
+    calcular_consumo()
+main()
