7a3b4e7cc090895c86cb6dc0d50a3065f9f96b17
Renato-Miranda <renatomna@gmail.com>
2025-07-01 20:18:31 -0300

    acrescentado lógica se o ano é centenário

--- Atividade_03/verificar_bissexto.py
+++ Atividade_03/verificar_bissexto.py
@@ -5,9 +5,8 @@
 
 def verificar_ano():
     ano = int(input("Digite o ano para verificar se é bissexto: "))
-    bissexto = ano % 4 == 0
 
-    if bissexto is True:
+    if (ano % 4 ==0 and ano % 100 !=0) or (ano % 400 == 0):
         print(f"O ano de {ano} é Bissexto!" )
     else:
         print(f"O ano de {ano} não é Bissexto!")
