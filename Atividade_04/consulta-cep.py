b8acad63665e2766fae511594fb3d5bc244c169d
Renato-Miranda <renatomna@gmail.com>
2025-07-02 19:19:18 -0300

    Chamada de API do Via CEP. tratamento de erros.

---
+++ Atividade_04/consulta-cep.py
@@ -0,0 +1,27 @@
+import requests
+
+def consultar_cep(cep):
+    cep = cep.strip().replace("-", "")
+    if not cep.isdigit() or len(cep) !=8:
+        print("CEP inválido! digite exatamente 8 números.")
+        return
+    
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+
+    try:
+        resposta = requests.get(url)
+        dados = resposta.json()
+
+        if "erro" in dados:
+            print("CEP não encontrado!")
+        else:
+            print(f"\n📍 Informações do CEP {cep}:")
+            print(f"Logradouro: {dados.get('logradouro', 'Não disponível')}")
+            print(f"Bairro: {dados.get('bairro', 'Não disponível')}")
+            print(f"Cidade: {dados.get('localidade', 'Não disponível')}")
+            print(f"Estado: {dados.get('uf', 'Não disponível')}")
+    except requests.exceptions.RequestException as e:
+        print("Erro de conexão: {e}")
+
+cep_usuario = input("Digite o CEP (apenas números): ")
+consultar_cep(cep_usuario)
