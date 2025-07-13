1cc98f27e538e16ae39bd93d33fa539f3e416c87
Renato-Miranda <renatomna@gmail.com>
2025-07-13 16:35:00 -0300

    criação lógica de leitura do arquivo csv que foi criado no programa 01 da atividade.

---
+++ Atividade_07/leitor-csv.py
@@ -0,0 +1,20 @@
+import csv
+
+# Nome do arquivo CSV a ser lido
+nome_arquivo = "pessoas.csv"
+
+# Lendo o arquivo CSV
+try:
+    with open(nome_arquivo, mode='r', encoding='utf-8') as arquivo_csv:
+        leitor = csv.DictReader(arquivo_csv)
+
+        print("Dados das pessoas:\n")
+        for linha in leitor:
+            nome = linha["Nome"]
+            idade = linha["Idade"]
+            cidade = linha["Cidade"]
+            print(f"Nome: {nome}, Idade: {idade}, Cidade: {cidade}")
+except FileNotFoundError:
+    print(f"Arquivo '{nome_arquivo}' não encontrado.")
+except Exception as e:
+    print(f"Ocorreu um erro ao ler o arquivo: {e}")
