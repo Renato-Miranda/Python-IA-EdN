17d53c65402c90e0d56a7e0ceeb908ad2e2df75c
Renato-Miranda <renatomna@gmail.com>
2025-09-10 19:19:06 -0300

    Execícos dos dia 2

---
+++ Mente_Lógica/dia2.py
@@ -0,0 +1,44 @@
+# nome = 'Renato'
+# idade = 33 
+# altura = 1.80
+# estudante = True
+
+# print("Nome:", nome)
+# print("idade:", idade)
+# print("altura:", altura)
+# print("estudante:", estudante)
+
+# ano_nascimento = 2025 - idade 
+# print("Ano de Nascimento:", ano_nascimento )
+
+# maior_de_idade = idade >=18
+# print("Maior de idade:", maior_de_idade)
+
+# frase = "Olá, Meu nome é " + nome + " e eu tenho " + str(idade) + " anos."
+# print(frase)
+
+# numero1 = float(input("Digite o primeiro numero: "))
+# numero2 = float(input("Digite o segundo numero: "))
+
+
+# soma = numero1 + numero2
+# subtracao = numero1 - numero2
+# multiplicacao = numero1 * numero2
+# divisao = numero1 / numero2
+
+# print("Soma: ", soma)
+# print("subtracao: ", subtracao)
+# print("multiplicacao: ", multiplicacao)
+# print("divisao: ", divisao)
+
+# --------------------------- Conversor de temparaturas
+
+# celcius = float(input("Digite uma temperatura em Celcius: "))
+# fahrenheit = celcius * 9/5 + 32
+# print("A temperatura em Fahrenheit é:", fahrenheit)
+
+# ------------------------- Área de um círculo
+raio = float(input("Digite o raio do círculo: "))
+pi = 3.14159
+area = pi * raio ** 2
+print(f"A área do círculo é: {area:.2f}")
