9f2e55c6458b9c320b361742175961d044496835
Renato-Miranda <renatomna@gmail.com>
2025-09-12 15:46:30 -0300

    Ordem crescente e decrescente com lista

--- Mente_Lógica/dia6.py
+++ Mente_Lógica/dia6.py
@@ -1,8 +1,48 @@
 # 1 - Lista de convidados
-convidados = ["Renata", "Henrique", "Felipe", "Bruna"]
+# convidados = ["Renata", "Henrique", "Felipe", "Bruna"]
 
-for convidado in convidados:
-    print(f"{convidado}, seja bem-vinds a minha festa!")
+# for convidado in convidados:
+#     print(f"{convidado}, seja bem-vinds a minha festa!")
 
 # 2 - Estatística de Números
 
+# entrada = input("Digite números separados por espaço: ")
+# numeros = [float(num) for num in entrada.split()]
+
+# maior_numero = max(numeros)
+# menor_numero = min(numeros)
+# soma_numeros = sum(numeros)
+# media_numeros = soma_numeros / len(numeros)
+
+# print("-------Estatíticas------------")
+# print("Maior número", maior_numero)
+# print("Menor número", menor_numero)
+# print("Soma dos números", soma_numeros)
+# print("Média dos números", media_numeros)
+
+# 3 - Contagem de Caracteres em uma String:
+
+# frase = str(input("Digite uma frase: "))
+# letras = {}
+
+# for caractere in frase:
+#     if caractere.isalpha():
+#         if caractere in letras:
+#             letras[caractere] += 1
+#         else:
+#             letras[caractere] = 1
+
+# for letra, contagem in letras.items():
+#     print(f"A letra '{letra}' aparece {contagem} vez(es)")
+
+# 4 - Ordenando uma Lista:
+
+entrada = input("Digite uma lista de números separados por espaço: ")
+numeros = [float(num) for num in entrada.split()]
+print(numeros)
+
+numeros_crescente = sorted(numeros)
+print("Numeros em ordem crescente:", numeros_crescente)
+
+numeros_decrescente = sorted(numeros, reverse=True)
+print("Números em ordem decrescente:", numeros_decrescente)
