cff67aeae99b48d60dc64f58257f748853910b6f
Renato-Miranda <renatomna@gmail.com>
2025-09-12 14:01:52 -0300

    Lista de convidados

--- Mente_Lógica/dia6.py
+++ Mente_Lógica/dia6.py
@@ -1,1 +1,8 @@
+# 1 - Lista de convidados
 convidados = ["Renata", "Henrique", "Felipe", "Bruna"]
+
+for convidado in convidados:
+    print(f"{convidado}, seja bem-vinds a minha festa!")
+
+# 2 - Estatística de Números
+
