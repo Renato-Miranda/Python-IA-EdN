4e3e62117bff35b49f1db6ee0fbd08efea646cc2
Renato-Miranda <renatomna@gmail.com>
2025-09-12 10:56:32 -0300

    Finalização exercícios dia 5 - Loops

--- Mente_Lógica/dia5.py
+++ Mente_Lógica/dia5.py
@@ -35,19 +35,77 @@
 # print("Impar:",impar)
 
 # 5 Adivinhe o Número:
-import random
+# import random
 
-programa = random.randint(1,101)
-tentativas = 0
+# programa = random.randint(1,101)
+# tentativas = 0
 
-while True:
-    palpite = int(input("Digite um palpite de 1 a 100: "))
-    tentativas += 1
-    if palpite == programa:
-        print(f"Parabéns! Você acertou em {tentativas} tentativas.")
-        break
-    elif palpite < programa:
-        print("O número é maior!")
+# while True:
+#     palpite = int(input("Digite um palpite de 1 a 100: "))
+#     tentativas += 1
+#     if palpite == programa:
+#         print(f"Parabéns! Você acertou em {tentativas} tentativas.")
+#         break
+#     elif palpite < programa:
+#         print("O número é maior!")
+#     else:
+#         print("O número é menor!")
+
+#################################### Extras
+
+# Calculando Fatorial de um número
+
+# N = int(input("Digite um número inteiro para descobrir o seu Fatorial: "))
+
+# multiplicacao = 1
+
+# for i in range(1, N + 1):
+#     multiplicacao *= i
+
+# print(multiplicacao)
+
+# sequencia Fibonacci
+
+# N = int(input("Digite um número inteiro: "))
+
+# termo1 = 0
+# termo2 = 1
+# contador = 0 
+
+# if N <=0 :
+#     print("Digite um número positivo")
+# elif N == 1:
+#     print("Serie Fibonacci até", N, "Termo:")
+#     print(termo1)
+# else:
+#     print("Serie Fibonacci:")
+#     while contador < N:
+#         print(termo1)
+#         proximo_termo = termo1 + termo2
+#         termo1 = termo2
+#         termo2 = proximo_termo
+#         contador +=1
+
+# Jogo da forca:
+
+palavra_secreta = "machado"
+letras_descobertas = ["_"] * len(palavra_secreta)
+tentativas = 7
+
+while tentativas > 0 and "_" in letras_descobertas:
+    print("Palavra:", " ".join(letras_descobertas))
+    letra = input("Digite uma letra: ").lower()
+
+    if letra in palavra_secreta:
+        for idx, letra_secreta in enumerate(palavra_secreta):
+            if letra == letra_secreta:
+                letras_descobertas[idx] = letra
+                print("Boa! Você acertou uma letra.")
     else:
-        print("O número é menor!")
+        tentativas -= 1
+        print(f"Errou! Você tem {tentativas} tentativas restantes.")
 
+if "_" not in letras_descobertas:
+    print("Parabéns! você adivinhou a palavra:", palavra_secreta)
+else:
+    print("Você perdeu! A palavra era:", palavra_secreta)
