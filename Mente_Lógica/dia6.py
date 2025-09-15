b727ff0ed95cbc15c946b06375eb0851fc592813
Renato-Miranda <renatomna@gmail.com>
2025-09-15 10:49:18 -0300

    Exercício Analisador de notas.

--- Mente_Lógica/dia6.py
+++ Mente_Lógica/dia6.py
@@ -62,37 +62,71 @@
 
 # Lista de tarefas
 
-tarefas = []
+# tarefas = []
 
-while True:
-    print("\nMEnu de Tarefas:")
-    print("1. Adicionar Tarefa")
-    print("2. Remover Tarefa")
-    print("3. Listar Tarefa")
-    print("4. Sair")
+# while True:
+#     print("\nMEnu de Tarefas:")
+#     print("1. Adicionar Tarefa")
+#     print("2. Remover Tarefa")
+#     print("3. Listar Tarefa")
+#     print("4. Sair")
 
-    opcao = input("Escolha uma opção: ")
+#     opcao = input("Escolha uma opção: ")
 
-    if opcao == "1":
-        tarefa = input("Digite a tarefa a ser adicionada: ")
-        tarefas.append(tarefa)
-        print("Tarefa adicionada com sucesso.")
-    elif opcao == "2":
-        tarefa = input("Digite a tarefa a ser removida: ")
-        if tarefa in tarefas:
-            tarefas.remove(tarefa)
-            print("Tarefa removida com sucesso.")
-        else:
-            print("Tarefa não encontrada.")
-    elif opcao == "3":
-        print("\nLista de Tarefas:")
-        for idx, tarefa in enumerate(tarefas, start=1):
-            print(f"{idx}. {tarefa}")
-    elif opcao == "4":
-        print("Saindo do programa.")
-        break
-    else:
-        print("Opção inválida. Tente novamente.")
+#     if opcao == "1":
+#         tarefa = input("Digite a tarefa a ser adicionada: ")
+#         tarefas.append(tarefa)
+#         print("Tarefa adicionada com sucesso.")
+#     elif opcao == "2":
+#         tarefa = input("Digite a tarefa a ser removida: ")
+#         if tarefa in tarefas:
+#             tarefas.remove(tarefa)
+#             print("Tarefa removida com sucesso.")
+#         else:
+#             print("Tarefa não encontrada.")
+#     elif opcao == "3":
+#         print("\nLista de Tarefas:")
+#         for idx, tarefa in enumerate(tarefas, start=1):
+#             print(f"{idx}. {tarefa}")
+#     elif opcao == "4":
+#         print("Saindo do programa.")
+#         break
+#     else:
+#         print("Opção inválida. Tente novamente.")
 
-    
+# Analisador de notas:
+
+# notas = []
+
+
+
+# while True:
+
+#     print("\n Menu notas")
+#     print("1. Adicionar nota")
+#     print("2 Sair")
+
+#     opcao = input("Digite uma opção: ")
+
+#     if opcao == "1":
+#         nota = float(input("Digite uma nota: "))
+#         notas.append(nota)
+#     elif opcao == "2":
+#         print("Encerrando adicionamento de notas.")
+#         break
+#     else:
+#         print("Opção inválida.")
+
+
+# if notas:
+#     print(notas)
+#     maior = max(notas)
+#     menor = min(notas)
+#     soma = sum(notas)
+#     media = soma / len(notas)
+#     print(f"A maior nota da turma é: {maior}") 
+#     print(f"A menor nota da turma é: {menor}")
+#     print(f"A média da turma é: {media}")
+# else:
+#     print("A lista de notas está vazia.")       
           
