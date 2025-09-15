cef6579e13101cbd60b0653d927e8087587da539
Renato-Miranda <renatomna@gmail.com>
2025-09-15 10:05:03 -0300

    Exercício do lista de tarefas

--- Mente_Lógica/dia6.py
+++ Mente_Lógica/dia6.py
@@ -37,12 +37,62 @@
 
 # 4 - Ordenando uma Lista:
 
-entrada = input("Digite uma lista de números separados por espaço: ")
-numeros = [float(num) for num in entrada.split()]
-print(numeros)
+# entrada = input("Digite uma lista de números separados por espaço: ")
+# numeros = [float(num) for num in entrada.split()]
+# print(numeros)
 
-numeros_crescente = sorted(numeros)
-print("Numeros em ordem crescente:", numeros_crescente)
+# numeros_crescente = sorted(numeros)
+# print("Numeros em ordem crescente:", numeros_crescente)
 
-numeros_decrescente = sorted(numeros, reverse=True)
-print("Números em ordem decrescente:", numeros_decrescente)
+# numeros_decrescente = sorted(numeros, reverse=True)
+# print("Números em ordem decrescente:", numeros_decrescente)
+
+# meses = ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro")
+# numero_mes = int(input("Digite um número de 1 a 12: "))
+
+
+# if numero_mes >= 1 and numero_mes <= 12:   # valida se está no intervalo correto
+#     print(meses[numero_mes - 1])  # ajusta índice (1 → 0, 2 → 1, etc.)
+# else:
+#     print("Número inválido!")
+
+# for i, mes in enumerate(meses):
+    # if i == numero_mes:
+    #     print(mes)
+
+# Lista de tarefas
+
+tarefas = []
+
+while True:
+    print("\nMEnu de Tarefas:")
+    print("1. Adicionar Tarefa")
+    print("2. Remover Tarefa")
+    print("3. Listar Tarefa")
+    print("4. Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        tarefa = input("Digite a tarefa a ser adicionada: ")
+        tarefas.append(tarefa)
+        print("Tarefa adicionada com sucesso.")
+    elif opcao == "2":
+        tarefa = input("Digite a tarefa a ser removida: ")
+        if tarefa in tarefas:
+            tarefas.remove(tarefa)
+            print("Tarefa removida com sucesso.")
+        else:
+            print("Tarefa não encontrada.")
+    elif opcao == "3":
+        print("\nLista de Tarefas:")
+        for idx, tarefa in enumerate(tarefas, start=1):
+            print(f"{idx}. {tarefa}")
+    elif opcao == "4":
+        print("Saindo do programa.")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
+
+    
+          
