029aff7a51a7b5298a5e3a843b740f8b2a6e8d4a
Renato-Miranda <renatomna@gmail.com>
2025-09-17 11:28:25 -0300

    Exercício com módulo os, manipulação de pastas e arquivos no sistema operacional.

--- Mente_Lógica/dia9.py
+++ Mente_Lógica/dia9.py
@@ -29,3 +29,24 @@
 #             tentativas -= 1
 
 # jogo_adivinhação()
+
+import os
+
+def manipular_arquivos():
+    #Criando uma pasta
+    os.mkdir('nova_pasta')
+    print("Pasta 'nova_pasta' criada.")
+
+    #mudando para novo diretório
+    os.chdir('nova_pasta')
+
+    #criando um arquivo
+    with open('arquivo.txt', 'W') as arquivo:
+        arquivo.write("Este é um arquivo dentro da nova pasta.")
+        print("Arquivo 'arquivo.txt' criado dentro de 'nova_pasta'.")
+
+    # Listando arquivos
+    arquivos = os.listdir('.')
+    print("Arquivos no diretório atual:", arquivos)
+
+manipular_arquivos()
