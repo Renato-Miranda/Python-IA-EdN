4a8d8dd6452c38e375153bd29a91bd29b61892ee
Renato-Miranda <renatomna@gmail.com>
2025-09-17 14:39:24 -0300

    Exercício invertendo palavras.

--- Mente_Lógica/dia10.py
+++ Mente_Lógica/dia10.py
@@ -12,3 +12,13 @@
 #     print(f"Essa frase contêm {contador} vogais.")
 # else:
 #     print("Não sei como, mas essa frase não tem vogais")
+
+# # Invertendo Palavras:
+
+# frase = input("Digite uma frase a ser invertida: ")
+# frase_invertida = ''.join(reversed(frase))
+# # ou
+# frase_invertida2 = frase[::-1]
+
+# print(f"Frase invertida usando Reversed: {frase_invertida}")
+# print(f"Frase invertida usando [::-1]: {frase_invertida2}")
