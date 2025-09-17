44bfc66c0cff6e0e56880584dbfa403dabecfa19
Renato-Miranda <renatomna@gmail.com>
2025-09-17 14:28:59 -0300

    Exercícios com strings: contador de vogais.

---
+++ Mente_Lógica/dia10.py
@@ -0,0 +1,14 @@
+# frase = input("Digite uma frase para contar as vogais: ").lower()
+# vogais = 'aeiou'
+# contador = 0
+
+
+# for letra in frase:
+#     if letra in vogais:
+#         contador += 1
+
+
+# if contador > 0:   
+#     print(f"Essa frase contêm {contador} vogais.")
+# else:
+#     print("Não sei como, mas essa frase não tem vogais")
