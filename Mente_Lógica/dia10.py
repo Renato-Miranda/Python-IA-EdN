3fe9a76444d651dc1bd59d78942f27272c6692fd
Renato-Miranda <renatomna@gmail.com>
2025-09-17 15:04:10 -0300

    Exercício: Verificador de senha forte.

--- Mente_Lógica/dia10.py
+++ Mente_Lógica/dia10.py
@@ -22,3 +22,32 @@
 
 # print(f"Frase invertida usando Reversed: {frase_invertida}")
 # print(f"Frase invertida usando [::-1]: {frase_invertida2}")
+
+# frase = str(input("Digite usa palavra ou frase: ")).lower()
+# frase_sem_espaco = frase.replace(" ", "")
+# print(frase_sem_espaco)
+# frase_invertida = ''.join(reversed(frase_sem_espaco))
+# print(frase_invertida)
+
+# if frase_invertida == frase_sem_espaco:
+#     print("É um palíndromo.")
+# else:
+#     print("Não é um palíndromo.")
+
+# senha forte:
+
+import string
+
+senha = input("Digite uma senha forte: ")
+
+tem_maisculas = any(c.isupper() for c in senha)
+tem_minusculas = any(c.islower() for c in senha)
+tem_numeros = any(c.isdigit() for c in senha)
+simbolos = any(not c.isalnum() for c in senha)
+tamanho_adequado = len(senha) >= 8
+
+if tem_maisculas and tem_minusculas and tem_numeros and simbolos and tamanho_adequado:
+    print("Sua senha é forte!")
+else:
+    print("Sua senha é fraca. Tente novamente")
+
