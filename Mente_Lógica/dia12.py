0a9d033d4cce55a5d3e8ae09f9fea5f9c2f06b43
Renato-Miranda <renatomna@gmail.com>
2025-09-18 14:50:33 -0300

    Tratamento de Exceções: Calculadora

---
+++ Mente_Lógica/dia12.py
@@ -0,0 +1,35 @@
+# 1 Calculadora de funções
+
+# def somar(a, b):
+#     return a + b
+
+# def subtrair(a, b):
+#     return a - b
+
+# def multiplicar(a, b):
+#     return a * b
+
+# def dividir(a, b):
+#     if b != 0:
+#         return  a / b
+#     else:
+#         return "Erro: Divisão por zero!"
+# try:    
+#     numero1 = float(input("Digite o primeiro número: "))
+#     operacao = input("Digite a operação (+ , - , * , /): ")
+#     numero2 = float(input("Digite o segundo número: "))
+
+#     if operacao == "+":
+#         resultado = somar(numero1, numero2)
+#         print(f"Resulta da soma entre {numero1} + {numero2} é igual: {resultado}")
+#     elif operacao == "-":
+#         resultado = subtrair(numero1, numero2)
+#         print(f"Resulta da subtração entre {numero1} - {numero2} é igual: {resultado}")
+#     elif operacao == "*":
+#         resultado = multiplicar(numero1, numero2)
+#         print(f"Resulta da multiplicação entre {numero1} * {numero2} é igual: {resultado}")
+#     elif operacao == "/":
+#         resultado = dividir(numero1, numero2)
+#         print(f"Resulta da divisão entre {numero1} / {numero2} é igual: {resultado}")
+# except (ValueError , ZeroDivisionError) as e:
+#     print(f"Erro: {e}")
