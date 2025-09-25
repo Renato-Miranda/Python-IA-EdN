b8deaf6b395e25a030feaa3be2aa004b82a71500
Renato-Miranda <renatomna@gmail.com>
2025-09-25 14:04:46 -0300

    Gerenciador de Contatos

--- Mente_Lógica/dia13.py
+++ Mente_Lógica/dia13.py
@@ -120,3 +120,87 @@
 #             escritor.writerow(aluno)
 #         print("Médias calculadas e salvas no arquivo 'medias.cv'")
 
+# Exercícios Práticos: 
+
+#1 - Contador de Palavras em um Arquivo.
+
+# import csv
+
+# nome_arquivo = input("Digite o nome do arquivo: ")
+
+# try:
+#     with open(nome_arquivo, 'r') as arquivo:
+#         conteudo = arquivo.read()
+# except FileNotFoundError:
+#     print("Arquivo não encontrado.")
+# else:
+#     palavras = conteudo.lower().split()
+#     contagem = {}
+
+#     for palavra in palavras:
+#         palavra = palavra.strip('.,!?";:-()')
+#         if palavra in contagem:
+#             contagem[palavra] += 1
+#         else:
+#             contagem[palavra] = 1
+
+# for palavra, quantidade in contagem.items():
+#     print(f"{palavra}: {quantidade}")
+
+# 2 - Gerenciador de Contatos
+
+# import csv
+
+# def adicionar_contato():
+#     nome = input("Nome: ")
+#     telefone = input("Telefone: ")
+#     email = ("Email: ")
+
+#     with open('contatos.csv', 'a', newline='') as arquivo_csv:
+#         escritor = csv.writer(arquivo_csv)
+#         escritor.writerow([nome, telefone, email])
+#     print("Contato adicionado com sucesso.")
+
+# def listar_contato():
+#     try:
+#         with open('contatos.csv', 'r') as arquivo_csv:
+#             leitor = csv.reader(arquivo_csv)
+#             for linha in leitor:
+#                 print(f"Nome: {linha[0]}, telefone: {linha[1]}, Email: {linha[2]}")
+#     except FileNotFoundError:
+#         print("Nenhum contado encontrado.")
+
+
+# while True:
+#     print("Menu:")
+#     print("1. Adicionar contato")
+#     print("2. Listar contato")
+#     print("3. Sair")
+
+#     opcao = input("Escolha uma opção: ")
+
+#     if opcao == '1':
+#         adicionar_contato()
+#     elif opcao == '2':
+#         listar_contato()
+#     elif opcao == '3':
+#         break
+#     else: 
+#         print("Opção inválida.")
+
+
+# 3 Leitor de Arquivos JSON
+
+import json
+
+try:
+    with open('produtos.json', 'r') as arquivo_json:
+        produtos = json.load(arquivo_json)
+except FileNotFoundError:
+    print("Arquivo não encontrado.")
+else:
+    for produto in produtos:
+        print(f"Nome: {produto['nome']}")
+        print(f"Preço: R${produto['preco']:.2f}")
+        print(f"Quantidade: {produto['quantidade']}")
+
